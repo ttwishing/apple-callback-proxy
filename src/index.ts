6724b64de332040879df2214a8f17e0ab3a11fbc
kurt <ttwishing@gmail.com>
2025-07-24 14:36:08 +0800

    redirect to auth/callback/apple

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,30 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+interface Env {
+	SVELTEKIT_URL: string;
+	ALLOWED_ORIGIN: string;
+}
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: Env): Promise<Response> {
+		if (request.method !== 'POST' || new URL(request.url).pathname !== '/apple-callback-proxy') {
+			return new Response('Method or path not allowed', { status: 405 });
+		}
+
+		try {
+			const formData: FormData = await request.formData();
+
+			const code = formData.get('code');
+			const state = formData.get('state');
+			if (!code || !state) {
+				return new Response('Missing code or state', { status: 400 });
+			}
+			const url = new URL(env.SVELTEKIT_URL);
+			url.searchParams.append('code', encodeURIComponent(code as string));
+			url.searchParams.append('state', encodeURIComponent(state as string));
+			console.log('Redirecting browser to:', url.toString());
+			return Response.redirect(url.toString(), 302);
+		} catch (err) {
+			console.error('Proxy error:', err);
+			return new Response('Proxy failed', { status: 500 });
+		}
 	},
-} satisfies ExportedHandler<Env>;
+};
